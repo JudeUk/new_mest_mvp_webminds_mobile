d7a74bbd91ea3e82aa706ca83e9ec83cd82b9843
moby1804 <mogbitseo@yahoo.com>
2024-01-29 22:16:17 +0000

    moving files to team repo

---
+++ lib/mentor_list_pages/mentor_profile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MentorProfile extends StatelessWidget {
+  const MentorProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.purple[100],
+    );
+  }
+}
