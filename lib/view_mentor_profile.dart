e8a9905ddb7bd90bc7b0981937e0649bf1dca50b
Akweley Okai <akweleyokaiofficial@gmail.com>
2024-01-29 21:51:25 +0000

    added view mentor profile new

--- lib/view_mentor_profile.dart
+++ lib/view_mentor_profile.dart
@@ -1,84 +1,289 @@
 import 'package:flutter/material.dart';
 
-class MyProfilePage extends StatefulWidget {
-  const MyProfilePage({super.key, required this.title});
-
-  final String title;
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
 
   @override
-  State<MyProfilePage> createState() => _MyProfilePageState();
+  State<ProfilePage> createState() => _ProfilePageState();
 }
 
-class _MyProfilePageState extends State<MyProfilePage> {
-  List<String> feedItems = [
-    "Bright",
-    "Abena",
-    "Grace",
-    "Joe",
-    "Jason",
-  ];
-  final double coverHeight = 280;
-  final double profileHeight = 140;
 
+class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  height: coverHeight,
-                  width: double.infinity,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Image.network(
-                      "https://images.unsplash.com/photo-1683009427500-71296178737f?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                      fit: BoxFit.cover,
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Profile', style: TextStyle(color: Colors.black87),),
+        leading: Icon(Icons.menu, color: Colors.black87,),
+        backgroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage("https://images.unsplash.com/photo-1683009427500-71296178737f?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+                      )
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          child: Text('', style: TextStyle(color: Colors.grey[800], fontSize: 25, fontWeight: FontWeight.bold),),
+                        ),
+                        Container(
+                          child: Text('Full stack Developer', style: TextStyle(color: Colors.blueGrey[400], fontSize: 14, fontWeight: FontWeight.w600),),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              border: Border.all(width: 1, color: Colors.black),
+                          ),
+                          child: Center(
+                              child: Icon(Icons.message, color: Colors.blueGrey[400],)
+                          ),
+                        ),
+                        ],
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 20),
+                child: Divider(
+                  thickness: 1,
+                  color: Colors.blueGrey[200],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Text('203', style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),),
+                        ),
+                        Container(
+                          child: Text('Followers', style: TextStyle(fontSize: 15, color: Colors.blueGrey),),
+                        )
+                      ],
                     ),
                   ),
-                ),
-                CircleAvatar(
-                  radius: profileHeight / 2,
-                  backgroundImage: const NetworkImage(
-                      "https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-                ),
-                const SizedBox(
-                  height: 120,
-                ),
-                const Text('Yaw Essumang')
-              ],
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: feedItems.length,
-                itemBuilder: (BuildContext context, int position) {
-                  String name = feedItems[position];
-                  return _MyProfilePageView(name, position);
-                },
+                  Container(
+                    padding: EdgeInsets.only(top: 30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Text('932', style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),),
+                        ),
+                        Container(
+                          child: Text('Following', style: TextStyle(fontSize: 15, color: Colors.blueGrey),),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Text('30', style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),),
+                        ),
+                        Container(
+                          child: Text('Projects', style: TextStyle(fontSize: 15, color: Colors.blueGrey),),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _MyProfilePageView(String name, int position) {
-    return Card(
-      color: Colors.white,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      child: SizedBox(
-        height: 100,
-        child: ListTile(
-          title: Text(name),
-          trailing: const Icon(Icons.more_horiz),
+              Container(
+                padding: EdgeInsets.only(top: 20),
+                child: Divider(
+                  thickness: 1,
+                  color: Colors.blueGrey[200],
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 30),
+                    child: Text('Top Skills', style: TextStyle(fontSize: 20, color: Colors.blueGrey, fontWeight: FontWeight.w900),),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 20, left: 10, right: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.grey[300],
+                          ),
+                          child: Center(child: Text('Flutter', style: TextStyle(color: Colors.blueGrey),)),
+                        ),
+                        Container(
+                          width: 100,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.grey[300],
+                          ),
+                          child: Center(child: Text('React JS', style: TextStyle(color: Colors.blueGrey),)),
+                        ),
+                        Container(
+                          width: 100,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.grey[300],
+                          ),
+                          child: Center(child: Text('Node js', style: TextStyle(color: Colors.blueGrey),)),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 20, left: 10, right: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.grey[300],
+                          ),
+                          child: Center(child: Text('Django', style: TextStyle(color: Colors.blueGrey),)),
+                        ),
+                        Container(
+                          width: 100,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.grey[300],
+                          ),
+                          child: Center(child: Text('Laravel', style: TextStyle(color: Colors.blueGrey),)),
+                        ),
+                        Container(
+                          width: 100,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.grey[300],
+                          ),
+                          child: Center(child: Text('Express JS', style: TextStyle(color: Colors.blueGrey),)),
+                        ),
+                        Container(
+                          width: 100,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.grey[300],
+                          ),
+                          child: Center(child: Text('Tensorflow', style: TextStyle(color: Colors.blueGrey),)),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text('Projects', style: TextStyle(fontSize: 20, color: Colors.blueGrey, fontWeight: FontWeight.w900),),
+                    ),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 250,
+                            width: MediaQuery.of(context).size.width,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(30),
+                                child: Image.network("https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",)
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.only(top: 10, left: 10),
+                                child: Text('MERN Stack Project', style: TextStyle(color: Colors.blueGrey, fontWeight: FontWeight.bold, fontSize: 20),),
+                              ),
+                              Container(
+                                child: Icon(Icons.favorite, color: Colors.red,),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 50),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 250,
+                            width: MediaQuery.of(context).size.width,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(30),
+                                child: Image.network("https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.only(top: 10, left: 10),
+                                child: Text('Flutter vs React', style: TextStyle(color: Colors.blueGrey, fontWeight: FontWeight.bold, fontSize: 20),),
+                              ),
+                              Container(
+                                child: Icon(Icons.favorite, color: Colors.red,),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
