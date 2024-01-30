2a094628d8e4381daf6198a7371f6098b8d8b633
Judeukana@gmail.com <judeukana@Judes-MacBook-Pro.local>
2024-01-30 10:48:42 +0000

    View mentor screen, sign in modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:webminds_mobile/complete_mentor_profile.dart';
 import 'package:webminds_mobile/mentee_requests_screen.dart';
+import 'package:webminds_mobile/mentor_list_pages/mentor_view.dart';
 import 'package:webminds_mobile/mentor_list_pages/view_mentor.dart';
 import 'package:webminds_mobile/profile_type_screen.dart';
 import 'package:webminds_mobile/root.dart';
@@ -14,14 +15,16 @@ void main() {
   // ignore: prefer_const_constructors
   runApp( MaterialApp(
     
-    home: ProfilePage(),
+    // home: ProfilePage(),
     //  home:SignInPage(),
     // home: SignUpScreen(), // signup screen
 
     //    home: CompleteProfileScreen(), // complete mentor profile, refine with finishing touches
 
-          home: ScheduleScreen(),// mentor views list of mentees requests
-    // home: SignInScreen(),
+          // home: ScheduleScreen(),// mentor views list of mentees requests
+    home: SignInScreen(),
+    // home: ViewMentorProfile(), //Mentor Page to book Mentor
+    // home: MentorListPage(), // Mentees screen Listing mentors to pick from
     // home: rootApp() // practice screen, listview cards
   //  home: myAPPTwo(), //practice screen, stateless
     // home: ProfileTypeScreen(), // select profile type screen
--- lib/mentor_list_pages/mentor_home.dart
+++ lib/mentor_list_pages/mentor_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webminds_mobile/mentor_list_pages/view_mentor.dart';
 
 class MentorHome extends StatefulWidget {
   const MentorHome({Key? key}) : super(key: key);
@@ -96,6 +97,12 @@ class _MentorHomeState extends State<MentorHome> {
                         color: Colors.red[300],
                       ),
                       onTap: () {
+                         Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ViewMentorProfile(),
+                            ),
+                          );
                         // Add your onTap logic here
                       },
                     ),
